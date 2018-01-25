c4ac3f1cc9e014755c181b0c5f3da2fa11532c25
liealis <liealis@163.com>
2018-01-25 10:21:32 +0800

    upload Wx_product.php

---
+++ application/controllers/api/Wx_product.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kevin
+ * Date: 2018-01-24
+ * Time: 15:52
+ */
+
+class Wx_product extends CI_Controller
+{
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->helper('url');
+	}
+}
